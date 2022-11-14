b8a00d0b5ae22885e6f62bd7327b604a5f6fff6c
LM <josephguiyabc@gmail.com>
2022-11-15 00:09:45 +0800

    Added Polling

--- src/main/java/com/Pothole/app/controller/PotholeController.java
+++ src/main/java/com/Pothole/app/controller/PotholeController.java
@@ -3,6 +3,7 @@ package com.Pothole.app.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,12 +17,13 @@ import com.Pothole.app.service.PotholeService;
 @RestController
 @CrossOrigin
 public class PotholeController {
-	
+	int x=1;
 	@Autowired
 	private PotholeService service;
 	
 	@GetMapping("/GetPotholes")
 	public List<Pothole> findAllPotholes(){
+		x=0;
 		return service.getPotholes();
 	}
 	@GetMapping("/GetPot/{id}")
@@ -30,6 +32,17 @@ public class PotholeController {
 	}
 	@PostMapping("/PostPothole")
 	public Pothole addPothole(@RequestBody Pothole pothole){
+		x=1;
         return service.savePothole(pothole);
     }
+	@GetMapping("/GetActive")
+	public List<Pothole> potholeCount() {
+		if(x == 1) {
+			x=0;
+			System.out.println("Welcome");
+			return service.getPotholes();
+		}
+		System.out.println("qwaeeqw");
+		return null;
+	}
 }
